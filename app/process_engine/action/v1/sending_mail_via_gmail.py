42b8d67907c03ff34509af7dda349bd57df75327
Patryk Migaj <77615611+patromi@users.noreply.github.com>
2021-08-13 13:54:33 +0200

    Update sending_mail_via_gmail.py

--- app/process_engine/action/v1/sending_mail_via_gmail.py
+++ app/process_engine/action/v1/sending_mail_via_gmail.py
@@ -7,77 +7,52 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 import lxml
+from pydantic import BaseModel
+from typing import Optional
+
+class Configuration(BaseModel):
+    smtp : str
+    port : int
+    username : str
+    password : str
+    to : str
+    From : str
+    title: Optional[str] = ''
+    replyTo: Optional[str] = None
+    message: Optional[str] = ''
+    @staticmethod
+    def send(config):
+        """Create and configurate message container """
+        message = MIMEMultipart('alternative')
+        message['From'] = config.From
+        message['To'] = config.to
+        message['Subject'] = config.title
+        message.add_header('reply-to', config.replyTo)
+
+        """Cleaning self.message from HTML tags using bs4 """
+        clearmessage = BeautifulSoup(config.message, "lxml").text
+
+        """Creating two parts of message one with HTML tags one without"""
+        part1 = MIMEText(clearmessage, 'plain')
+        part2 = MIMEText(config.message, 'html')
+        message.attach(part1)
+        message.attach(part2)
+
+        """Creating a session with STMP protocol"""
+        session = smtplib.SMTP(config.smtp, config.port)
+        session.ehlo()
+        session.starttls()
+
+        """Entering login and password"""
+        session.login(config.username, config.password)
+        session.sendmail(config.From, config.to, message.as_string())
+        session.quit()
 class Gmail(ActionRunner):
-
     def __init__(self, *args, **kwargs):
-        self.error_status = False
         self.config = kwargs
-        """Check for empty fields"""
-        self.smtp = kwargs['smtp'] if 'smtp' in kwargs else None
-        if self.smtp == None:
-            self.smtp = 'Smtp no specified'
-            self.error_status = True
-        self.port = kwargs['port'] if 'port' in kwargs else None
-        if self.port == None:
-            self.port = 'Port not specified'
-            self.error_status = True
-        self.username = kwargs['username'] if 'username' in kwargs else None
-        if self.username == None:
-            self.username = 'No username specified'
-            self.error_status = True
-        self.password = kwargs['password'] if 'password' in kwargs else None
-        if self.password == None:
-            self.password = 'No password specified'
-            self.error_status = True
-        self.to = kwargs['to'] if 'to' in kwargs else None
-        if self.to == None:
-            self.to = 'To field not specified'
-            self.error_status = True
-        self.From = kwargs['from'] if 'from' in kwargs else None
-        if self.From == None:
-            self.From = 'From field not specified'
-            self.error_status = True
-        self.reply_to = kwargs['replyTo'] if 'replyTo' in kwargs else None
-        self.title = kwargs['title'] if 'title' in kwargs else ''
-        self.message = kwargs['message'] if 'message' in kwargs else ''
+        self.config = Configuration(**kwargs)
     async def run(self, void):
-        if self.error_status:
-            """Raise error if datas is empty"""
-            raise Exception(f'Incorrect configuration:\n'
-                            f'smtp: {self.smtp}\n'
-                            f'port: {self.port}\n'
-                            f'username: {self.username}\n'
-                            f'password: {self.password}\n'
-                            f'To: {self.to}\n'
-                            f'From: {self.From}\n'
-                            f'Reply_to: {self.reply_to}\n'
-                            )
-        else:
-            """Create and configurate message container """
-            message = MIMEMultipart('alternative')
-            message['From'] = self.From
-            message['To'] = self.to
-            message['Subject'] = self.title
-            message.add_header('reply-to', self.reply_to)
-
-            """Cleaning self.message from HTML tags using bs4 """
-            self.clearmessage = BeautifulSoup(self.message, "lxml").text
-
-            """Creating two parts of message one with HTML tags one without"""
-            part1 = MIMEText(self.clearmessage, 'plain')
-            part2 = MIMEText(self.message, 'html')
-            message.attach(part1)
-            message.attach(part2)
-
-            """Creating a session with STMP protocol"""
-            session = smtplib.SMTP(self.smtp, self.port)
-            session.ehlo()
-            session.starttls()
-
-            """Entering login and password"""
-            session.login(self.username, self.password)
-            session.sendmail(self.From,self.to, message.as_string())
-            session.quit()
+            Configuration.send(self.config)
             return Result(port='session', value='Mail Send!')
 def register() -> Plugin:
     return Plugin(
@@ -113,4 +88,3 @@ def register() -> Plugin:
             group=["Gmail"]
         )
     )
-
